d02721d571a57d8bd9d3443b67ff875fdc71c420
pullakp12 <pullakp12@gmail.com>
2017-04-06 12:03:46 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+var app = express();
+var fs = require('fs');
+var hbs = require('hbs');
+hbs.registerPartials(__dirname+'/views/partials');
+hbs.registerHelper('getCurrentYear',()=>{return new Date().getFullYear()} );
+hbs.registerHelper('scremIt',(text)=>{
+    return text.toUpperCase();
+})
+app.use(express.static(__dirname+'/public'));
+app.use((req,res,next)=>{
+    var now = new Date().toString();
+    var log =`${now} : ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('logs.txt',log+'\n',(error)=>{
+        if(error){
+            console.log("cannot write to the file");
+        }
+    });
+    next();
+});
+app.use((req,res,next)=>{
+    res.render('maintenance.hbs');
+})
+
+app.get('/',(req,res)=>{
+   res.render('welcome.hbs',{
+       pageTitle:'Home Page',
+       message:'Welcome to Home directory'
+   })
+});
+app.get('/about',(req,res)=>{
+
+    res.render('about.hbs',{
+        pageTitle:'About Page'.toLowerCase(),
+        year:new Date().getFullYear()
+    });
+});
+app.get('/bad',(req,res)=>{
+    res.send({
+        errorMessage : 'Unable to fullfilled the desired result'
+    });
+});
+
+app.listen(3000,()=>{
+    console.log("Sever is running at 3000 port");
+});
